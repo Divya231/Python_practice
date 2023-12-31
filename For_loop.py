0b9aa3f81456164b12f29f155d5d298a03681be2
Divya Gupta <dg379804@gmail.com>
2023-12-31 19:03:55 +0530

    For loop

---
+++ For_loop.py
@@ -0,0 +1,22 @@
+# For loop with range
+# range(5)
+# start=0
+# condition<5=4
+# increment +1
+
+# range(1,6)
+# start=1
+# condition 1 to 5
+# increment +1
+# range(1,10,2)
+# start =1
+# condition 1 to 10
+# increment +2
+
+# for num in range(10):
+#     print(num)
+
+# for num in range(1,10):
+#     print(num)
+for num in range(3,10,2):
+    print(num)
