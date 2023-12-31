22d727a432e6562e227e80c76b76e72f5c23e622
Divya Gupta <dg379804@gmail.com>
2023-12-31 18:40:32 +0530

    Conditional statement

---
+++ Conditional.py
@@ -0,0 +1,28 @@
+# If statement
+a=20
+if a%2==0:
+    print("a is even number")
+
+# if-else statement
+
+num=int(input("Enter number : "))
+if num%2==0:
+    print("num is even number")
+else:
+    print("num is odd")
+
+
+
+# If , elif , else
+    
+
+number=int(input("enter marks: "))
+if number<=40:
+    print("fail")
+elif number>=70:
+    print("Pass")
+else:
+    print("Error")
+
+    
+
