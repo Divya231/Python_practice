2c028a20f1df01a9f014976d39993e112c6856ad
Divya Gupta <dg379804@gmail.com>
2023-12-31 18:01:12 +0530

    data_type

--- data_type.py
+++ data_type.py
@@ -3,3 +3,34 @@ b="divya"
 c= 12.8
 d=2+3j
 print(type(a),type(b),type(c),type(d))
+
+# string
+
+S="Learning Python"
+s='''Python 
+     is 
+easy    
+     to 
+learn'''
+print(S)
+print(s)
+print(type(s),type(S))
+
+
+# List
+
+
+list=["Divya",100,"python","learning"]
+list[1]="Machine_learning"
+print(type(list))
+print(list)
+
+
+
+# Tuple
+
+
+tuple=("Data-science","Machine_learning",10,45,"Python")
+tuple[0]=1
+print(type(tuple))
+print(tuple)
