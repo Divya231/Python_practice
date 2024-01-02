bfeace20307f10deff1afee63a140ad266db257e
Divya Gupta <dg379804@gmail.com>
2024-01-02 17:53:39 +0530

    string indexing & slicing

---
+++ string_index_slicing.py
@@ -0,0 +1,6 @@
+s="Learning Python"
+print(s[-1])
+print(s[2])
+print(s[1:5])
+print(s[-1::-2])
+print(s[-1::-1])
--- while_loop.py
+++ while_loop.py
@@ -3,3 +3,12 @@ while i<=10:
     print( "Learning Day ",i)
     i=i+1
 print(i)
+
+
+# Reverse
+
+i=10
+while i>=1:
+    print("Learning Day ", i)
+    i=i-1
+print(i)
