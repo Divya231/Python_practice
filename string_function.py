a92201e80ab422c00751f65f70a8ab2c3d8ad1f1
Divya Gupta <dg379804@gmail.com>
2024-01-02 18:18:33 +0530

    string function

--- string_function.py
+++ string_function.py
@@ -3,3 +3,13 @@ print(s.title())
 print(s.capitalize())
 print(s.upper())
 print(s.lower())
+
+
+print(s.find("p"))
+print(s.find("n"))
+print(s.find("n,4"))
+
+print(s.index("a"))
+print(s.isalpha())
+print(s.isalnum())
+print(s.isdigit())
