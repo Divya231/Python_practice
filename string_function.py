68a18f9119dbe3678a4ac677a2a43a4312e248fd
Divya Gupta <dg379804@gmail.com>
2024-01-02 18:08:57 +0530

    string function

---
+++ string_function.py
@@ -0,0 +1,5 @@
+s="learning python"
+print(s.title())
+print(s.capitalize())
+print(s.upper())
+print(s.lower())
--- string_iteration.py
+++ string_iteration.py
@@ -10,3 +10,9 @@ print(s[i])
 
 for i in range(l-1,-1,-1):
     print(s[i])
+
+
+    # without len function
+
+for i in s:
+    print(i)
