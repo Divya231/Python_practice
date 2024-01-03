de9ae78a7fedfe4ada5f9a6116bb8bfafdbf59cf
Divya Gupta <dg379804@gmail.com>
2024-01-03 12:23:29 +0530

    string formatting

--- string_format.py
+++ string_format.py
@@ -6,3 +6,6 @@ print(s1,s2)
 n="hi my name is {0} , and my age is {1}".format("divya",22)
 p="hi my name is {1} , and my age is {0}".format("divya",22)
 print(n ,p)
+n="hi my name is {a} , and my age is {b}".format(a="divya",b=22)
+
+print(n)
