412b8b5aba8b8c68535ca6279f8e7ec1b39034aa
Divya Gupta <dg379804@gmail.com>
2024-01-03 12:21:20 +0530

    string formatting

---
+++ string_format.py
@@ -0,0 +1,8 @@
+s1="Learing python is {}".format("easy")
+s2="Learning Machine Learning is {}".format("interesting")
+print(s1,s2)
+
+
+n="hi my name is {0} , and my age is {1}".format("divya",22)
+p="hi my name is {1} , and my age is {0}".format("divya",22)
+print(n ,p)
