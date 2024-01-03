9a2d57bbcb61377add16bdc056ee29f391710c18
Divya Gupta <dg379804@gmail.com>
2024-01-03 13:09:13 +0530

    list iteration

---
+++ list_iteration.py
@@ -0,0 +1,11 @@
+l=["divya","44","2024","python","machine learning"]
+t=len(l)
+for i in range(t):
+    print(l[i])
+for i in l:
+    print(i)
+
+# reverse
+    
+for i in range(t-1,-1,-1):
+    print(l[i])
