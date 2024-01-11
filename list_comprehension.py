4fc09cbce6d1539c9b3ba1bd4960e8680cf62100
Divya Gupta <dg379804@gmail.com>
2024-01-11 22:11:51 +0530

    list

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,11 +1,7 @@
 list=[i*i for i in range(10) if i%2==0]
 print(list)
 
-#  
-# below list contains square of all odd numbers from  
-# range 1 to 10  
+ 
 
 
-odd=[a*2 for in range(10) if(a%2==1)]
-print(a)
    
---
+++ list_method.py
@@ -0,0 +1,26 @@
+l=[13,67,8,90,5,11,23,45,56,5]
+# l.append(200)
+# print(l)
+# # Ascending order
+# l.sort()  
+# print(l)
+# # Descending order
+# l.sort(reverse=True)
+# print(l)
+# l.reverse()
+# print(l)
+# c=l.index(5)
+# print(c)
+# d=l.count(5)
+# print(d)
+# m=l.copy()
+# m[0]=290
+# print(m)
+
+# l.insert(0,77)
+# print(l)
+m=[100,200,300,500]
+# l.extend(m)
+
+print(l+m)
+
