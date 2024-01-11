df41f4f1b1ac6c37e03d9d2b9cba281a84adc709
Divya Gupta <dg379804@gmail.com>
2024-01-12 00:02:17 +0530

    tuple

---
+++ tuple_ques.py
@@ -0,0 +1,16 @@
+tup=(1,43,56,4)
+print(type(tup),tup)
+
+# tup1=(1)
+# print(type(tup1),tup1)
+
+# tup1=(1,)
+# print(type(tup1),tup1)
+
+# tup[0]=23
+# print(tup)
+
+print(tup[0])
+print(tup[2])
+tup2=tup[1:]
+print(tup2)
