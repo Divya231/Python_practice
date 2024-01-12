f0b7338ed92a96e0404ec2f8408b1c8165c85daf
Divya Gupta <dg379804@gmail.com>
2024-01-12 19:40:27 +0530

    python

---
+++ for_loop_with_else.py
@@ -0,0 +1,12 @@
+# for i in range(5):
+#     print(i)
+# else:
+#     print("Sorry")
+
+
+for i in range(6):
+    print(i)
+    if i==3:
+        break
+else:
+    print("Srry")
---
+++ while_loop_with_else.py
@@ -0,0 +1,16 @@
+# i=0
+# while i<7:
+#     print(i)
+#     i=i+1
+# else:
+#     print("Else is printed")
+
+
+i=0
+while i<7:
+    print(i)
+    i=i+1
+    if i==5:
+        break
+else:
+    print("Let's check if else will be print")
