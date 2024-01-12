3747eaeb10746b904e0328fbe94cb714d9592eb6
Divya Gupta <dg379804@gmail.com>
2024-01-12 16:10:24 +0530

    python

---
+++ set.py
@@ -0,0 +1,5 @@
+var={1,"Divya","Gupta","Divya",1,23}
+print(var)
+print(type(var))
+for value in var:
+    print(value)
---
+++ set_intersection.py
@@ -0,0 +1,22 @@
+s1={1,2,5,6}
+s2={3,7,6,2}
+# print(s1.intersection(s2))  
+# common value in both set
+
+
+# isdisjoint
+# print(s1.isdisjoint(s2))
+# print(s1.issuperset(s2))
+# print(s1.issubset(s2))
+s1.add(54)
+print(s1)
+# l=list(s2)
+# print(type(l))
+(s1.update(s2))
+print(s1)
+
+print(s1.pop())
+print(s1)
+del(s1)
+s2.clear()
+print(s2)
