3b6c854939fd109d0043437430492b086808adfd
Divya Gupta <dg379804@gmail.com>
2024-01-13 16:12:55 +0530

    Import

---
+++ import.py
@@ -0,0 +1,34 @@
+# import math
+# s=math.sqrt(9)
+# print(s)
+
+# import math as m
+# s=m.sqrt(16)
+# print(s)
+
+# from math import sqrt,pow
+# s=sqrt(16)
+# p=pow(2,3)
+# print(s,p)
+
+
+
+# # Importing everything
+# from math import *
+
+
+# # dir function()
+# import math 
+# print(dir(math))
+# print(type(perm))
+
+
+# We can also create function and import them
+
+# from test_import import are,test
+# test()
+# print(are)
+
+import test_import as ti
+ti.test()
+print(ti.are)
---
+++ test_import.py
@@ -0,0 +1,5 @@
+from math import pi
+def test():
+    print("Hey , I'M test this is just for testing , how we can create our function and import them ")
+
+are="AREY|This is just for testing"
