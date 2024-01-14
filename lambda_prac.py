3a8b96961acd43237c8f8c236e2ea1aeff3f6069
Divya Gupta <dg379804@gmail.com>
2024-01-14 21:10:26 +0530

    Lambda practice

---
+++ lambda_prac.py
@@ -0,0 +1,22 @@
+# # Write a python program to generate a function to double a specified number.
+
+# # Example:
+
+# # Specified number: 11 Expected Answer:
+# # The double number of 11 = 22
+# a=int(input("Enter number you want to double"))
+# p=lambda a: a+a
+# print(p(a))
+
+
+# Write a python program to Multiply argument a with argument b and return the result:.
+
+# Example:
+
+# Specified number: a=10 and b=20
+#  Expected Answer:200
+
+n1=int(input("Enter number 1: "))
+n2=int(input("enter number 2: "))
+mul=lambda a,b:a*b
+print(mul(n1,n2))
