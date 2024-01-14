723864a641d0911b93a0066d0a707d612e0467f1
Divya Gupta <dg379804@gmail.com>
2024-01-14 21:20:05 +0530

    Operator practice

---
+++ operator_prac.py
@@ -0,0 +1,15 @@
+# What is the result of each of the following statements?
+
+# int(False)
+# int(True)
+# bool(0)
+# bool(1)
+# True + 2
+# float(True)
+
+print(bool("2017") - True)
+print(bool("") - False)
+print(bool(0) - False)
+print(bool() - False)
+print(bool("  ") - True)
+print(True-True - 0 )
