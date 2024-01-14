7d92ddd13a35900243b05723daf1743b81d9dee9
Divya Gupta <dg379804@gmail.com>
2024-01-14 21:40:46 +0530

    Practice ques

---
+++ list_comprehension_prac.py
@@ -0,0 +1,5 @@
+
+# Using list comprehension, print the odd numbers between 0 and 100.
+
+new=[i for i in range(0,100) if i%2==1]
+print("Oddd numbers ", new)
---
+++ string_prac.py
@@ -0,0 +1,9 @@
+# In a given string, substitute the word "green" with "blue"
+# Original string
+original_string = "The grass is green and the sky is blue. I love the green color."
+
+# Substitute "green" with "blue"
+modified_string = original_string.replace("green", "blue")
+
+# Print the modified string
+print(modified_string)
