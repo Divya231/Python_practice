67c96ef152aaab0fa7e997f61db70e0ccc7c6e14
Divya Gupta <dg379804@gmail.com>
2024-01-14 22:46:06 +0530

    List comprehension practice

--- list_comprehension_prac.py
+++ list_comprehension_prac.py
@@ -1,5 +1,7 @@
 
 # Using list comprehension, print the odd numbers between 0 and 100.
 
-new=[i for i in range(0,100) if i%2==1]
-print("Oddd numbers ", new)
+# new=[i for i in range(0,100) if i%2==1]
+# print("Oddd numbers ", new)
+
+
